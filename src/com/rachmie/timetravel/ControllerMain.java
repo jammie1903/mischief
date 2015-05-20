059fe072203829f3c4d720a3cd4398121f042b25
jammie1903 <jamiepennell@live.com>
2015-05-20 10:12:15 +0100

    layout update

--- src/com/rachmie/timetravel/ControllerMain.java
+++ src/com/rachmie/timetravel/ControllerMain.java
@@ -14,14 +14,14 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.util.Duration;
 
 public class ControllerMain implements Initializable {
-
-    @FXML
-    private ImageView wormhole;
 
     @FXML
     private TextField yearEntry;
@@ -29,10 +29,29 @@ public class ControllerMain implements Initializable {
     @FXML
     private ComboBox<String> monthSelector;
 
+    @FXML
+    private ImageView wormhole1;
+
+    @FXML
+    private Label eventText;
+
+    @FXML
+    private ImageView wormhole;
+
+    @FXML
+    private VBox messagePane;
+
+    @FXML
+    private StackPane timeMachineResultPane;
+
     private Map<String, Integer> months = new HashMap<>();
 
     @FXML
     void startTimeTravel(ActionEvent event) {
+
+        if (!yearIsValid()) {
+            popupAngryMechanic();
+        }
 
         LocalDate.parse("21/12/2014", DateTimeFormatter.ofPattern("dd/MM/yyyy"));
         RotateTransition rt = new RotateTransition(Duration.millis(3000), wormhole);
@@ -40,6 +59,26 @@ public class ControllerMain implements Initializable {
         rt.setAutoReverse(false);
         rt.setInterpolator(Interpolator.EASE_BOTH);
         rt.play();
+    }
+
+    @FXML
+    void resetTimeMachine(ActionEvent event) {
+
+    }
+
+    private void popupAngryMechanic() {
+        // TODO Auto-generated method stub
+
+    }
+
+    private boolean yearIsValid() {
+        try {
+            int year = Integer.parseInt(yearEntry.getText());
+            //july 1991 - january - 2093
+            return year > 1990 && year < 2094;
+        } catch (Exception ex) {
+            return false;
+        }
     }
 
     @Override
